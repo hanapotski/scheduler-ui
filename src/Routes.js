27e2d2799d25a86a40fbd92ca0934b87d8bb612b
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2020-07-04 16:54:08 -0700

    Setup routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Sched from './views/Sched';
+import AllEvents from './views/AllEvents';
+import Home from './views/Home';
+import Event from './views/Event';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/sched">
-          <Sched />
+        <Route path="/events">
+          <AllEvents />
+        </Route>
+        <Route path="/events/:id">
+          <Event />
+        </Route>
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </Router>
