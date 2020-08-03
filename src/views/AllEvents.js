026142383527f52c6dc6a8048fa3f73dab182280
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2020-08-02 17:46:00 -0700

    Add all events component

---
+++ src/views/AllEvents.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Event from './Event';
+
+const mockData = [
+  {
+    id: '123',
+    createdBy: 'hannah',
+    modifiedBy: null,
+    modifiedDate: null,
+    eventDate: new Date(),
+    eventTitle: 'Sunday service',
+    leader: 'Tin',
+    backups: ['alyssa', 'mark'],
+    keyboardist: 'hannah',
+    guitarists: [
+      { name: 'euly', type: 'acoustic' },
+      { name: 'derick', type: 'electric' },
+    ],
+    drummer: 'zion',
+    other: [{ name: 'lloyd', instrument: 'percussion' }],
+  },
+];
+export default () => {
+  return (
+    <>
+      <h1>Schedule</h1>
+      {mockData.map((event) => (
+        <Event {...event} />
+      ))}
+    </>
+  );
+};
