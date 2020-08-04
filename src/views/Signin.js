f7df7094aee24b3b46747d1cee43d40c94d084b4
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2020-08-03 19:16:05 -0700

    Add styles to sign in page

--- src/views/Signin.js
+++ src/views/Signin.js
@@ -32,25 +32,62 @@ export default ({ setLogin }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <input
-        value={email}
-        onChange={({ target }) => setEmail(target.value)}
-        id="email"
-        type="email"
-        placeholder="Email"
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        value={password}
-        onChange={({ target }) => setPassword(target.value)}
-        id="password"
-        type="password"
-        placeholder="Password"
-      />
-      <button type="submit">Sign In</button>
-      <button onClick={() => history.push('/signup')}>Create an Account</button>
-    </form>
+    <div className="w-full max-w-xs m-4">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
+        <div class="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            id="email"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            id="password"
+            type="password"
+            placeholder="Password"
+          />
+          {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            onClick={() => history.push('/signup')}
+          >
+            Create an Account
+          </button>
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+      <p class="text-center text-gray-500 text-xs">
+        &copy;2020 HomemadeCoder. All rights reserved.
+      </p>
+    </div>
   );
 };
