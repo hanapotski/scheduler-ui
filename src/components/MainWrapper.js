da009447b31ed4fb3b0d4b383e90aafa2a95eb98
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2020-08-27 18:03:34 -0700

    Rename header

--- src/components/MainWrapper.js
+++ src/components/MainWrapper.js
@@ -4,7 +4,7 @@ export default function MainWrapper({ children }) {
   return (
     <div className="container mx-auto">
       <h1 className="text-center m-8 mb-0 text-blue-500 text-6xl font-extrabold">
-        PAW Schedule
+        Orani PAW Schedule
       </h1>
       <div className="flex justify-center">{children}</div>
     </div>
