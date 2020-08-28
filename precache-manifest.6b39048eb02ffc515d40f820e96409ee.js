66df91282c1224781fb4a3aa48b8e58b4028d4a5
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2020-08-27 17:52:16 -0700

    Create gh-pages with static content

---
+++ precache-manifest.6b39048eb02ffc515d40f820e96409ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf497d4df80623e672dfdc928cea0801",
+    "url": "/scheduler-ui/index.html"
+  },
+  {
+    "revision": "99576ef70330a19b5287",
+    "url": "/scheduler-ui/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "d649ff6efda4ff462a53",
+    "url": "/scheduler-ui/static/css/main.79ff5ed5.chunk.css"
+  },
+  {
+    "revision": "99576ef70330a19b5287",
+    "url": "/scheduler-ui/static/js/2.e0b2f478.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/scheduler-ui/static/js/2.e0b2f478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d649ff6efda4ff462a53",
+    "url": "/scheduler-ui/static/js/main.b1b24cc7.chunk.js"
+  },
+  {
+    "revision": "65bb6468a01eb439d629",
+    "url": "/scheduler-ui/static/js/runtime-main.d1a5e4e3.js"
+  }
+]);
--- src/helpers/constants.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL =
-  process.env.REACT_APP_BASE_URL || 'https://localhost:8000';
